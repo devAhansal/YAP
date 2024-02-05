223a81c1578c96a3ee6060dd96e695d5706ed656
R-ELKADAH <rachidelkadah@email.com>
2024-02-05 12:09:56 +0100

    conseil et porpos de nous

--- yap-vue/src/views/ConseilsView.vue
+++ yap-vue/src/views/ConseilsView.vue
@@ -4,17 +4,76 @@
     <div
       class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 flex justify-center items-center flex-wrap"
     >
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
-      <card-vent />
+      <Panel header="Conseil 01" toggleable>
+        <p class="m-0">
+          Évaluation du Mode de Vie : Choisissez un animal dont les besoins
+          correspondent à votre mode de vie. Certains animaux nécessitent plus
+          d'exercice, d'attention, ou d'espace que d'autres.
+        </p>
+      </Panel>
+      <br />
+      <Panel header="Conseil 02" toggleable>
+        <p class="m-0">
+          Taille de l'Espace : Considérez la taille de votre domicile. Certains
+          animaux, comme les chiens de grande taille, peuvent avoir besoin de
+          plus d'espace pour s'épanouir.
+        </p>
+      </Panel>
+      <br />
+      <Panel header="Conseil 03" toggleable>
+        <p class="m-0">
+          Niveau d'Activité : Sélectionnez un animal dont le niveau d'activité
+          correspond au vôtre. Certains animaux ont besoin d'exercice quotidien
+          tandis que d'autres sont plus adaptés à un style de vie plus
+          tranquille.
+        </p>
+      </Panel>
+      <br />
+
+
+      <Panel header="Conseil 04" toggleable>
+        <p class="m-0">
+          Disponibilité en Ressources : Évaluez si vous avez les ressources
+          nécessaires en termes de temps, d'argent, et d'énergie pour prendre
+          soin de l'animal choisi.
+        </p>
+      </Panel>
+      <br />
+
+      <Panel header="Conseil 05" toggleable>
+        <p class="m-0">
+          Compatibilité Familiale : Considérez la dynamique familiale. Certains
+          animaux peuvent être plus adaptés aux enfants, tandis que d'autres
+          peuvent nécessiter une attention plus exclusive.
+        </p>
+      </Panel>
+      <br />
+
+      <Panel header="Conseil 06" toggleable>
+        <p class="m-0">
+          Besoins Spécifiques : Informez-vous sur les besoins spécifiques de
+          l'espèce choisie. Certains animaux ont des exigences alimentaires,
+          médicales, ou environnementales spécifiques.
+        </p>
+      </Panel>
+      <br />
+
+      <Panel header="Conseil 07" toggleable>
+        <p class="m-0">
+          Temps Consacré à l'Animal : Évaluer le temps que vous pouvez consacrer
+          à l'animal chaque jour. Certains animaux nécessitent plus
+          d'interaction et de soins que d'autres.
+        </p>
+      </Panel>
+      <br />
+
+      <Panel header="Conseil 08" toggleable>
+        <p class="m-0">
+          Envisagez l'Adoption : Si possible, envisagez d'adopter un animal
+          auprès d'un refuge ou d'une organisation de sauvetage. Cela offre une
+          chance à un animal de trouver un foyer aimant.
+        </p>
+      </Panel>
     </div>
   </main>
 </template>
--- yap-vue/src/views/ProposView.vue
+++ yap-vue/src/views/ProposView.vue
@@ -1,23 +1,39 @@
 <template>
-    <navbar-client  routeCurrentName="propos"/>
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 flex justify-center items-center flex-wrap" >
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
-        <card-vent />
+  <navbar-client routeCurrentName="propos" />
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 flex justify-center items-center flex-wrap">
+      <div class="mb-8 ml-4 w-full max-w-1/2 bg-white border border-gray-200 rounded-lg shadow white:bg-gray-800 white:border-gray-700 h-[30%]">
+        <a href="#">
+          <img
+            class="p-8 rounded-t-lg" style="height: 600px;width: 100%;" 
+            src="C:\xampp\htdocs\YAP\yap-vue\public\images\cute-little-dog-pet-shop-with-owner.jpg"
+            alt="product image"
+          />
+        </a>
+        <div class="px-5 pb-5">
+          <a href="#">
+            <h5
+              class="text-xl font-semibold tracking-tight text-gray-500 dark:text-dark"
+            >
+            Chez YAP, notre mission est d'apporter joie, bonheur et responsabilité dans chaque foyer en connectant les amoureux des animaux avec des compagnons fidèles. Nous nous engageons à offrir une expérience d'adoption transparente et éthique, mettant en avant le bien-être des animaux.
+            </h5>
+            <h5
+              class="text-xl font-semibold tracking-tight text-gray-500 dark:text-dark"
+            >
+            Nous nous efforçons de garantir le bien-être de chaque animal que nous proposons à l'adoption. Chaque compagnon de YAP est traité avec amour, respect et reçoit des soins de qualité.            </h5>
+            <h5
+              class="text-xl font-semibold tracking-tight text-gray-500 dark:text-dark"
+            >
+            Notre vision est de devenir la référence en matière d'adoption d'animaux en ligne, en créant des foyers heureux et épanouis où les humains et les animaux partagent des liens indéfectibles.            </h5>
+          </a>
+
+          <div class="flex items-center justify-between"></div>
+        </div>
       </div>
-    </main>
-  </template>
-  
-  <script setup>
-  import NavbarClient from "../components/NavbarClient.vue";
-  </script>
-  
+    </div>
+  </main>
+</template>
+
+<script setup>
+import NavbarClient from "../components/NavbarClient.vue";
+</script>
